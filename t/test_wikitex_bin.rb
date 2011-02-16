3b3ba8d32b293248b0c6808f1d4921b717ae39f7
Akira FUNAI <akira@funai.com>
2011-02-16 16:48:52 +0900

    TC_WikiTeX_Bin#test_init

--- t/test_wikitex_bin.rb
+++ t/test_wikitex_bin.rb
@@ -4,16 +4,26 @@
 # Copyright:: Copyright (c) 2011 Akira FUNAI
 
 require "#{::File.dirname __FILE__}/t"
+require 'fileutils'
 
 class TC_WikiTeX_Bin < Test::Unit::TestCase
 
   def setup
+    @tmp_dir = ::File.expand_path('./tmp', ::File.dirname(__FILE__))
+    ::FileUtils.rm_r ::Dir.glob("#{@tmp_dir}/*")
   end
 
   def teardown
   end
 
   def test_init
+    ::Dir.chdir(@tmp_dir) do
+      system('../../bin/wikitex init foobar 2>/dev/null')
+      assert(
+        ::File.exists?('./foobar'),
+        'wikitex command should copy skeltons to the target directory'
+      )
+    end
   end
 
 end
